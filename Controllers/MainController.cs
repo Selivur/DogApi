2b3aaa1dc2ea5f2be353ab8156803963af30ee99
Illia <si01082001@gmail.com>
2023-06-21 16:18:53 +0300

    Modified DTOs and add MainController

---
+++ Controllers/MainController.cs
@@ -0,0 +1,56 @@
+﻿using DogApi.Model.DbContext;
+using DogApi.Model.Dto;
+using DogApi.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace DogApi.Controllers
+{
+    [ApiController]
+    public class MainController : Controller
+    {
+        private readonly DogContext _context;
+        public MainController(DogContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("dogs")]
+        public async Task<IActionResult> QueriedDogs([FromQuery] GetQueriedDogsRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorMessages = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+                return BadRequest(errorMessages);
+            }
+            return Ok(_context.Dogs);
+        }
+        [HttpPost]
+        [Route("dog")]
+        public async Task<IActionResult> CreateDog([FromBody] CreateDogRequest dog)
+        {          
+            if (!ModelState.IsValid)
+            {
+                var errorMessages = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+                return BadRequest(errorMessages);
+            }
+            Dog dogs = new Dog
+            {
+                Name = dog.Name,
+                Color = dog.Color,
+                TailLength = dog.TailLength,
+                Weight = dog.Weight
+            };
+            _context.Add(dogs);
+            _context.SaveChanges();
+            return Created("/dog", dog);
+
+        }
+    }
+}
--- Model/Dto/GetQueriedDogsResponse.cs
+++ Model/Dto/GetQueriedDogsResponse.cs
@@ -1,6 +1,19 @@
-﻿namespace DogApi.Model.Dto
+﻿using System.Text.Json.Serialization;
+
+namespace DogApi.Model.Dto
 {
     public class GetQueriedDogsResponse
     {
+        [JsonPropertyName("attribute")]
+        public string? Atribute { get; set; }
+
+        [JsonPropertyName("order")]
+        public string? Order { get; set; }
+
+        [JsonPropertyName("pageNumber")]
+        public int? PageNumber { get; set; }
+
+        [JsonPropertyName("pageSize")]
+        public int? PageSize { get; set; }
     }
 }
